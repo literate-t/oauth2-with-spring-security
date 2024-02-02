230a7b2cabeca90a71c0b8cb3e3f6eadbbf67291
literate <gaekoon@gmail.com>
2024-02-02 10:58:06 +0900

    Initial commit

---
+++ src/main/java/io/security/oauth2withspringsecurity/Oauth2WithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.oauth2withspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2WithSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/oauth2withspringsecurity/Oauth2WithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oauth2withspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2WithSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
