e0705a01d5f7684a5f087bcc1bd6defddaf7cb45
literate <gaekoon@gmail.com>
2024-02-03 23:49:26 +0900

    Feat: OAuth2 로그인 때 로그인 페이지 추가

---
+++ src/main/java/io/security/oauth2withspringsecurity/LoginController.java
@@ -0,0 +1,12 @@
+package io.security.oauth2withspringsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
