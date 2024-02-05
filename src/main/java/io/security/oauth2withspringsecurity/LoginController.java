88997b5e08103ad52547525907c6e1bcd0157fba
literate <gaekoon@gmail.com>
2024-02-06 02:58:30 +0900

    Feat: baseUri 변경
    - authorizationEndpoint.baseUri()는 최초 권한 부여의 시작 uri를 변경한다(임시 코드 발급받는 엔드포인트) 디폴트값은 /oauth2/authorization/{registrationId}이며 클라이언트에서의 로그인 링크와 일치해야 한다
    
    - redirectionEndpointConfig.baseUri()는 access token을 발급받을 때 필요한 callback uri를 변경한다. 인가서버에도 추가해야하고 application.yml에도 동기화해놔야 한다

--- src/main/java/io/security/oauth2withspringsecurity/LoginController.java
+++ src/main/java/io/security/oauth2withspringsecurity/LoginController.java
@@ -1,12 +1,13 @@
 package io.security.oauth2withspringsecurity;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
--- src/main/java/io/security/oauth2withspringsecurity/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2withspringsecurity/OAuth2ClientConfig.java
@@ -1,43 +1,49 @@
 package io.security.oauth2withspringsecurity;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
-
-    public OAuth2ClientConfig(ClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(
-            authRequest -> authRequest.anyRequest().authenticated());
-        http.oauth2Login(Customizer.withDefaults());
-        http.logout()
-            .logoutSuccessHandler(oidcLogoutSuccessHandler())
-            .invalidateHttpSession(true)
-            .clearAuthentication(true)
-            .deleteCookies("JSESSIONID");
+            (requests) -> requests.antMatchers("/login").permitAll().anyRequest().authenticated());
+        http.oauth2Login(oauth2 -> oauth2.loginPage("/login")
+            .authorizationEndpoint(
+                authorizationEndpointConfig -> authorizationEndpointConfig.baseUri(
+                    "/oauth2/v1/authorization")) // "/oauth2/authorization" -> default
+            .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig.baseUri(
+                // yml 파일에도 반영되어야 하고 인거서버에도 반영되어야 함
+                "/login/v1/oauth2/code/*")) // "/login/oauth2/code/*" -> default
+        );
 
         return http.build();
     }
 
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(
-            clientRegistrationRepository);
-
-        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-
-        return successHandler;
-    }
+    // oidcLogoutSuccessHandler
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests(
+//            authRequest -> authRequest.anyRequest().authenticated());
+//        http.oauth2Login(Customizer.withDefaults());
+//        http.logout()
+//            .logoutSuccessHandler(oidcLogoutSuccessHandler())
+//            .invalidateHttpSession(true)
+//            .clearAuthentication(true)
+//            .deleteCookies("JSESSIONID");
+//
+//        return http.build();
+//    }
+//    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+//        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(
+//            clientRegistrationRepository);
+//
+//        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+//
+//        return successHandler;
+//    }
 }
